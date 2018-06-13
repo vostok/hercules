778d5c4b26dfb13f36b13ce2d8c0b9d611092ae6
Kirill Sulim <sulim@skbkontur.ru>
2018-06-13 13:24:19 +0500

    Make SizeOf a static class

--- hercules-protocol/src/main/java/ru/kontur/vostok/hercules/protocol/decoder/SizeOf.java
+++ hercules-protocol/src/main/java/ru/kontur/vostok/hercules/protocol/decoder/SizeOf.java
@@ -1,15 +1,19 @@
 package ru.kontur.vostok.hercules.protocol.decoder;
 
-public interface SizeOf {
-    int BYTE = 1;
-    int SHORT = 2;
-    int INTEGER = 4;
-    int LONG = 8;
-    int FLAG = 1;
-    int FLOAT = 4;
-    int DOUBLE = 8;
-    int STRING_LENGTH = 1;
-    int TEXT_LENGTH = 4;
-    int VECTOR_LENGTH = 1;
-    int ARRAY_LENGTH = 4;
+public final class SizeOf {
+    public final int BYTE = 1;
+    public final int SHORT = 2;
+    public final int INTEGER = 4;
+    public final int LONG = 8;
+    public final int FLAG = 1;
+    public final int FLOAT = 4;
+    public final int DOUBLE = 8;
+    public final int STRING_LENGTH = 1;
+    public final int TEXT_LENGTH = 4;
+    public final int VECTOR_LENGTH = 1;
+    public final int ARRAY_LENGTH = 4;
+
+    
+    private SizeOf() {
+    }
 }
