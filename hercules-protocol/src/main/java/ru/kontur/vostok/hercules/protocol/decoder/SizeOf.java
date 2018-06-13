bc56fdee67ab8834b5e68494ac478be898cfdf1f
Kirill Sulim <sulim@skbkontur.ru>
2018-06-13 13:30:14 +0500

    Add forgotten static in SizeOf

--- hercules-protocol/src/main/java/ru/kontur/vostok/hercules/protocol/decoder/SizeOf.java
+++ hercules-protocol/src/main/java/ru/kontur/vostok/hercules/protocol/decoder/SizeOf.java
@@ -1,19 +1,19 @@
 package ru.kontur.vostok.hercules.protocol.decoder;
 
 public final class SizeOf {
-    public final int BYTE = 1;
-    public final int SHORT = 2;
-    public final int INTEGER = 4;
-    public final int LONG = 8;
-    public final int FLAG = 1;
-    public final int FLOAT = 4;
-    public final int DOUBLE = 8;
-    public final int STRING_LENGTH = 1;
-    public final int TEXT_LENGTH = 4;
-    public final int VECTOR_LENGTH = 1;
-    public final int ARRAY_LENGTH = 4;
+    public static final int BYTE = 1;
+    public static final int SHORT = 2;
+    public static final int INTEGER = 4;
+    public static final int LONG = 8;
+    public static final int FLAG = 1;
+    public static final int FLOAT = 4;
+    public static final int DOUBLE = 8;
+    public static final int STRING_LENGTH = 1;
+    public static final int TEXT_LENGTH = 4;
+    public static final int VECTOR_LENGTH = 1;
+    public static final int ARRAY_LENGTH = 4;
 
-    
+
     private SizeOf() {
     }
 }
