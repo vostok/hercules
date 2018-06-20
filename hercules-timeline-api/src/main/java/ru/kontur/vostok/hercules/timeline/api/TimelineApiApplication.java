3c821455a55965e523615c2015a92e0c4056c8b6
Kirill Sulim <sulim@skbkontur.ru>
2018-06-20 17:04:26 +0500

    Create scaffold for timeline-api

---
+++ hercules-timeline-api/src/main/java/ru/kontur/vostok/hercules/timeline/api/TimelineApiApplication.java
@@ -0,0 +1,48 @@
+package ru.kontur.vostok.hercules.timeline.api;
+
+import ru.kontur.vostok.hercules.auth.AuthManager;
+import ru.kontur.vostok.hercules.util.args.ArgsParser;
+import ru.kontur.vostok.hercules.util.properties.PropertiesUtil;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class TimelineApiApplication {
+
+    private static HttpServer server;
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        try {
+            Map<String, String> parameters = ArgsParser.parse(args);
+
+            Properties httpServerProperties = PropertiesUtil.readProperties(parameters.getOrDefault("httpserver.properties", "httpserver.properties"));
+
+            server = new HttpServer(httpServerProperties, new AuthManager());
+            server.start();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            shutdown();
+            return;
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(TimelineApiApplication::shutdown));
+
+        System.out.println("Stream API started for " + (System.currentTimeMillis() - start) + " millis" );
+    }
+
+    private static void shutdown() {
+        long start = System.currentTimeMillis();
+        System.out.println("Started Stream API shutdown");
+        try {
+            if (server != null) {
+                server.stop();
+            }
+        } catch (Throwable e) {
+            e.printStackTrace(); //TODO: Process error
+        }
+
+        System.out.println("Finished Stream API shutdown for " + (System.currentTimeMillis() - start) + " millis");
+    }
+}
