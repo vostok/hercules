70583777394bb128d8bbdc1d0236f853c1804ec5
Gregory <kgn@skbkontur.ru>
2018-06-27 15:57:44 +0500

    Add UUID and time conversion test.

---
+++ hercules-uuid/src/test/java/ru/kontur/vostok/hercules/uuid/UuidTest.java
@@ -0,0 +1,28 @@
+package ru.kontur.vostok.hercules.uuid;
+
+import org.junit.Test;
+import ru.kontur.vostok.hercules.util.time.TimeUtil;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Gregory Koshelev
+ */
+public class UuidTest {
+    @Test
+    public void testTimestampExtraction() {
+        UUID uuid = UUID.fromString("50554d6e-29bb-11e5-b345-feff819cdc9f");
+        long ticks = uuid.timestamp();
+        long timestamp = TimeUtil.gregorianTicksToUnixTime(ticks);
+        Instant instant = Instant.ofEpochMilli(timestamp);
+        OffsetDateTime date = OffsetDateTime.ofInstant(instant, ZoneOffset.UTC);
+        assertEquals(2015, date.getYear());
+        assertEquals(7, date.getMonthValue());
+        assertEquals(14, date.getDayOfMonth());
+    }
+}
