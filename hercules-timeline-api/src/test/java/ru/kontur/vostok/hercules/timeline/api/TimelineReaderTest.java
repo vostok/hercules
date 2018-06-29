846c43e0b8a0e234ca3d1e67ed8c32a49421da3a
Kirill Sulim <sulim@skbkontur.ru>
2018-06-29 14:04:17 +0500

    Add simple TimelineReader test

--- hercules-timeline-api/src/test/java/ru/kontur/vostok/hercules/timeline/api/TimelineReaderTest.java
+++ hercules-timeline-api/src/test/java/ru/kontur/vostok/hercules/timeline/api/TimelineReaderTest.java
@@ -1,17 +1,13 @@
 package ru.kontur.vostok.hercules.timeline.api;
 
-import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.SimpleStatement;
-import com.datastax.driver.core.Statement;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.ArgumentMatcher;
+import org.mockito.internal.matchers.CompareEqual;
+import org.mockito.internal.matchers.Matches;
 import ru.kontur.vostok.hercules.cassandra.util.CassandraConnector;
 import ru.kontur.vostok.hercules.meta.timeline.Timeline;
 import ru.kontur.vostok.hercules.protocol.TimelineByteContent;
@@ -21,59 +17,91 @@ import ru.kontur.vostok.hercules.protocol.TimelineShardReadState;
 import java.util.Arrays;
 import java.util.Iterator;
 
-import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.matches;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class TimelineReaderTest {
+
+    private static class StatementMatcher implements ArgumentMatcher<SimpleStatement> {
+
+        private final ArgumentMatcher<Object> cqlMatcher;
+        private final String regexp;
+
+        public StatementMatcher(String regexp) {
+            this.cqlMatcher = new Matches(regexp);
+            this.regexp = regexp;
+        }
+
+        @Override
+        public boolean matches(SimpleStatement argument) {
+            return cqlMatcher.matches(argument.getQueryString());
+        }
+
+        @Override
+        public String toString() {
+            return "Expression matches \"" + regexp + "\"";
+        }
+    }
 
     private static final Timeline TIMELINE = new Timeline();
     static {
         TIMELINE.setName("test-timeline");
-        TIMELINE.setSlices(2);
-        TIMELINE.setTimetrapSize(10_000);
+        TIMELINE.setSlices(1);
+        TIMELINE.setTimetrapSize(1_000);
     }
 
     private TimelineReader timelineReader;
-    private Session session = Mockito.mock(Session.class);
-    private ResultSet resultSet = Mockito.mock(ResultSet.class);
+    private Session session = mock(Session.class);
+    private ResultSet resultSet = mock(ResultSet.class);
 
     @Before
     public void setUp() throws Exception {
-        CassandraConnector connector = Mockito.mock(CassandraConnector.class);
-        Mockito.when(connector.session()).thenReturn(session);
+        CassandraConnector connector = mock(CassandraConnector.class);
+        when(connector.session()).thenReturn(session);
 
         mockIterable(resultSet);
 
-        Mockito.when(session.execute(Mockito.any(SimpleStatement.class))).thenReturn(resultSet);
+        when(session.execute(any(SimpleStatement.class))).thenReturn(resultSet);
 
         timelineReader = new TimelineReader(connector);
     }
 
     @Test
-    public void shouldRequestSomething() {
+    public void shouldRequestTwoSlices() {
+        Timeline timeline = new Timeline();
+        timeline.setName("test-timeline");
+        timeline.setSlices(2);
+        timeline.setTimetrapSize(1_000);
 
-        TimelineByteContent content = timelineReader.readTimeline(
-                TIMELINE,
+        timelineReader.readTimeline(
+                timeline,
                 new TimelineReadState(new TimelineShardReadState[]{}),
                 0,
                 1,
                 10,
                 0,
-                100_000
+                1_000
         );
 
-
+        verify(session).execute(argThat(cql(".+ slice = 0 .+")));
+        verify(session).execute(argThat(cql(".+ slice = 1 .+")));
     }
 
     public static <T> void mockIterable(Iterable<T> iterable, T... values) {
-        Iterator<T> mockIterator = Mockito.mock(Iterator.class);
-        Mockito.when(iterable.iterator()).thenReturn(mockIterator);
+        Iterator<T> mockIterator = mock(Iterator.class);
+        when(iterable.iterator()).thenReturn(mockIterator);
 
         if (values.length == 0) {
-            Mockito.when(mockIterator.hasNext()).thenReturn(false);
+            when(mockIterator.hasNext()).thenReturn(false);
             return;
         } else if (values.length == 1) {
-            Mockito.when(mockIterator.hasNext()).thenReturn(true, false);
-            Mockito.when(mockIterator.next()).thenReturn(values[0]);
+            when(mockIterator.hasNext()).thenReturn(true, false);
+            when(mockIterator.next()).thenReturn(values[0]);
         } else {
             // build boolean array for hasNext()
             Boolean[] hasNextResponses = new Boolean[values.length];
@@ -81,9 +109,13 @@ public class TimelineReaderTest {
                 hasNextResponses[i] = true;
             }
             hasNextResponses[hasNextResponses.length - 1] = false;
-            Mockito.when(mockIterator.hasNext()).thenReturn(true, hasNextResponses);
+            when(mockIterator.hasNext()).thenReturn(true, hasNextResponses);
             T[] valuesMinusTheFirst = Arrays.copyOfRange(values, 1, values.length);
-            Mockito.when(mockIterator.next()).thenReturn(values[0], valuesMinusTheFirst);
+            when(mockIterator.next()).thenReturn(values[0], valuesMinusTheFirst);
         }
+    }
+
+    public static StatementMatcher cql(String regexp) {
+        return new StatementMatcher(regexp);
     }
 }
