3a2ff6c2f6e90a384773d5ca01301886cb0c2e16
Mahetov Sergey <mahetov@skbkontur.ru>
2018-07-19 13:36:10 +0500

    Add docs

---
+++ docs/createDoc.bat
@@ -0,0 +1,4 @@
+REM https://openapi-converter.herokuapp.com/
+REM https://github.com/Rebilly/ReDoc/blob/master/cli/README.md
+REM https://www.npmjs.com/package/bootprint-openapi
+redoc-cli bundle management-api-swagger2.yaml
