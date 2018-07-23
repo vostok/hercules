4dfcae5f7abdbd4d4fa3921200f2361379d83a3d
Kirill Sulim <kirillsulim@gmail.com>
2018-07-23 18:43:07 +0500

    Get rid of star import

--- hercules-meta/src/main/java/ru/kontur/vostok/hercules/meta/curator/CuratorClient.java
+++ hercules-meta/src/main/java/ru/kontur/vostok/hercules/meta/curator/CuratorClient.java
@@ -8,7 +8,11 @@ import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.data.Stat;
 import ru.kontur.vostok.hercules.util.properties.PropertiesUtil;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 /**
