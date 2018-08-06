9cbcece48728675177d00011bd748a40e7d827a2
jdk <jdk@skbkontur.ru>
2018-08-06 17:19:39 +0500

    Created HerculesMetricConfigFactory. Added tests for that. Changed ConfigsUtil in hercules-util module.

---
+++ hercules-micrometer-registry/src/main/java/ru/kontur/vostok/hercules/micrometer/registry/HerculesMetricConfigFactory.java
@@ -0,0 +1,85 @@
+package ru.kontur.vostok.hercules.micrometer.registry;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
+import ru.kontur.vostok.hercules.util.properties.ConfigsUtil;
+
+/**
+ * @author Daniil Zhenikhov
+ */
+public class HerculesMetricConfigFactory {
+    private static final String DEFAULT_RESOURCE_NAME = "hercules-metric.properties";
+    private static final String PROPERTY_NAME = "hercules.metric.config";
+
+    private static final String DEFAULT_CONFIG_PATH;
+    private static final boolean DEFAULT_CONFIG_PATH_EXIST;
+
+    static {
+        DEFAULT_CONFIG_PATH = System.getProperty(PROPERTY_NAME);
+        DEFAULT_CONFIG_PATH_EXIST = Objects.nonNull(DEFAULT_CONFIG_PATH);
+    }
+
+    /**
+     * Create {@link ru.kontur.vostok.hercules.micrometer.registry.HerculesMetricConfig HerculesMetricConfig} from resource folder
+     * with name <code>resourceName</code>
+     *
+     * @param resourceName name of resource containing configs
+     * @return {@link ru.kontur.vostok.hercules.micrometer.registry.HerculesMetricConfig HerculesMetricConfig}
+     * @throws IOException exception while loading config
+     */
+    public static HerculesMetricConfig fromResource(String resourceName) throws IOException {
+        InputStream inputStream =  ConfigsUtil.readConfig(resourceName, true);
+        Properties properties = loadProperties(inputStream);
+
+        return properties::getProperty;
+    }
+
+    /**
+     * Create {@link ru.kontur.vostok.hercules.micrometer.registry.HerculesMetricConfig HerculesMetricConfig} from resource folder
+     * with name {@value DEFAULT_RESOURCE_NAME}
+     *
+     * @return {@link ru.kontur.vostok.hercules.micrometer.registry.HerculesMetricConfig HerculesMetricConfig}
+     * @throws IOException exception while loading config
+     */
+    public static HerculesMetricConfig fromResource() throws IOException {
+        return fromResource(DEFAULT_RESOURCE_NAME);
+    }
+
+    /**
+     * Create {@link ru.kontur.vostok.hercules.micrometer.registry.HerculesMetricConfig HerculesMetricConfig}
+     * from specific file
+     * with name <code>filename</code>
+     *
+     * @return {@link ru.kontur.vostok.hercules.micrometer.registry.HerculesMetricConfig HerculesMetricConfig}
+     * @throws IOException exception while loading config
+     */
+    public static HerculesMetricConfig fromFile(String filename) throws IOException {
+        InputStream inputStream = ConfigsUtil.readConfig(filename, false);
+        Properties properties = loadProperties(inputStream);
+
+        return properties::getProperty;
+    }
+
+    /**
+     * Create {@link ru.kontur.vostok.hercules.micrometer.registry.HerculesMetricConfig HerculesMetricConfig}
+     * from specific file. Path to file defined in system property {@value PROPERTY_NAME}
+     *
+     * @return {@link ru.kontur.vostok.hercules.micrometer.registry.HerculesMetricConfig HerculesMetricConfig}
+     * @throws IOException exception while loading config
+     */
+    public static HerculesMetricConfig fromFile() throws IOException {
+        if (DEFAULT_CONFIG_PATH_EXIST) {
+            return fromFile(DEFAULT_CONFIG_PATH);
+        }
+        throw new FileNotFoundException(String.format("File '%s' has not found", DEFAULT_CONFIG_PATH));
+    }
+
+    private static Properties loadProperties(InputStream inputStream) throws IOException {
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        return properties;
+    }
+}
--- hercules-util/src/main/java/ru/kontur/vostok/hercules/util/properties/ConfigsUtil.java
+++ hercules-util/src/main/java/ru/kontur/vostok/hercules/util/properties/ConfigsUtil.java
@@ -9,6 +9,15 @@ import java.util.Objects;
  * @author Daniil Zhenikhov
  */
 public final class ConfigsUtil {
+    /**
+     * Read config file.
+     * <br>If propertyName exists then try to read from path specified in property.
+     * <br>Else try to load from resources.
+     * @param propertyName name of property containing path to config file
+     * @param defaultResource name of default resources containing config file
+     * @return {@link java.io.InputStream input stream} of config file
+     * @throws java.lang.RuntimeException wrapping exception that was occurred while trying to load config
+     */
     public static InputStream readConfig(String propertyName, String defaultResource) {
         String filename = System.getProperty(propertyName);
         boolean fromResources = false;
@@ -17,16 +26,28 @@ public final class ConfigsUtil {
             fromResources = true;
         }
         try {
-            return readProperties(filename, fromResources);
+            return readConfig(filename, fromResources);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static InputStream readProperties(String filename, boolean fromResources) throws FileNotFoundException {
+    /**
+     * Read config file.
+     * <br>
+     *
+     * @param filename name of config file. May be resource or path to config file.
+     * @param fromResources flag that config file is resource or not
+     * @return {@link java.io.InputStream input stream} of config file
+     * @throws FileNotFoundException if file is not resource and has not found, exception will be thrown
+     */
+    public static InputStream readConfig(String filename, boolean fromResources) throws FileNotFoundException {
         if (fromResources) {
             ClassLoader loader = ConfigsUtil.class.getClassLoader();
-            return loader.getResourceAsStream(filename);
+            InputStream inputStream = loader.getResourceAsStream(filename);
+
+            Objects.requireNonNull(inputStream, "Can not load resource " + filename);
+            return inputStream;
         }
         return new FileInputStream(filename);
 
