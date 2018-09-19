eb64873d643327e7898f247b63f014beecade09e
Kirill Sulim <sulim@skbkontur.ru>
2018-09-19 17:36:00 +0500

    Some metrics added to graphite sink

---
+++ hercules-kafka-util/src/main/java/ru/kontur/vostok/hercules/kafka/util/processing/BulkSenderStat.java
@@ -0,0 +1,27 @@
+package ru.kontur.vostok.hercules.kafka.util.processing;
+
+/**
+ * BulkSenderStat
+ *
+ * @author Kirill Sulim
+ */
+public class BulkSenderStat {
+
+    public static final BulkSenderStat ZERO = new BulkSenderStat(0, 0);
+
+    private final int processed;
+    private final int dropped;
+
+    public BulkSenderStat(int processed, int dropped) {
+        this.processed = processed;
+        this.dropped = dropped;
+    }
+
+    public int getProcessed() {
+        return processed;
+    }
+
+    public int getDropped() {
+        return dropped;
+    }
+}
