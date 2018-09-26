05124d29b7bdaadbe77e16eb6c5ae4fb38ec1dae
Kirill Sulim <sulim@skbkontur.ru>
2018-09-26 15:43:47 +0500

    Revert "Move initial read to separate method"
    This reverts commit 7bbcef5ce918cb181bfd8345bd96b216a7083c7a.

--- hercules-protocol/src/main/java/ru/kontur/vostok/hercules/protocol/decoder/ReaderIterator.java
+++ hercules-protocol/src/main/java/ru/kontur/vostok/hercules/protocol/decoder/ReaderIterator.java
@@ -3,32 +3,27 @@ package ru.kontur.vostok.hercules.protocol.decoder;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-/**
- * ReaderIterator<T> uses Reader<T> to read array of elements. Not thread safe.
- * @param <T>
- */
 public class ReaderIterator<T> implements Iterator<T> {
 
     private final Decoder decoder;
     private final Reader<T> elementReader;
-    private int total;
+    private final int total;
     private int remaining;
-    private boolean initialReadPerformed = false;
 
     public ReaderIterator(Decoder decoder, Reader<T> elementReader) {
         this.decoder = decoder;
         this.elementReader = elementReader;
+        this.total = decoder.readInteger();
+        this.remaining = total;
     }
 
     @Override
     public boolean hasNext() {
-        initialRead();
         return remaining > 0;
     }
 
     @Override
     public T next() {
-        initialRead();
         if (remaining <= 0) {
             throw new NoSuchElementException();
         }
@@ -39,15 +34,6 @@ public class ReaderIterator<T> implements Iterator<T> {
     }
 
     public int getTotal() {
-        initialRead();
         return total;
-    }
-
-    private void initialRead() {
-        if (!initialReadPerformed) {
-            this.total = decoder.readInteger();
-            this.remaining = total;
-            this.initialReadPerformed = true;
-        }
     }
 }
