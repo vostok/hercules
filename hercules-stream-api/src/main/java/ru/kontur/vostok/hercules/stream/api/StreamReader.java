b5f8a2add689be9d5c562b36fe78c94db8e562fa
Kirill Sulim <sulim@skbkontur.ru>
2018-10-22 13:05:09 +0500

    Readabilty fix

--- hercules-stream-api/src/main/java/ru/kontur/vostok/hercules/stream/api/StreamReader.java
+++ hercules-stream-api/src/main/java/ru/kontur/vostok/hercules/stream/api/StreamReader.java
@@ -119,7 +119,7 @@ public class StreamReader {
                     poll = new RecordStorage<>(0);
                 }
 
-                overflowedOffsets.forEach(offsetsToRequest::put);
+                offsetsToRequest.putAll(overflowedOffsets);
 
                 return new ByteStreamContent(
                         stateFromMap(streamName, offsetsToRequest),
