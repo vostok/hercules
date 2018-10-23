01c00f5f65728d10fc75e485a4f759f7c814d909
Kirill Sulim <sulim@skbkontur.ru>
2018-10-23 16:43:43 +0500

    Use PropertyDescritption in hercules-stream-api

--- hercules-stream-api/src/main/java/ru/kontur/vostok/hercules/stream/api/HttpServer.java
+++ hercules-stream-api/src/main/java/ru/kontur/vostok/hercules/stream/api/HttpServer.java
@@ -4,17 +4,32 @@ import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import ru.kontur.vostok.hercules.auth.AuthManager;
-import ru.kontur.vostok.hercules.util.properties.PropertiesExtractor;
+import ru.kontur.vostok.hercules.util.properties.PropertyDescription;
+import ru.kontur.vostok.hercules.util.properties.PropertyDescriptions;
+import ru.kontur.vostok.hercules.util.validation.Validators;
 
 import java.util.Properties;
 
 public class HttpServer {
 
+    private static class Props {
+        static final PropertyDescription<String> HOST = PropertyDescriptions
+                .stringProperty("host")
+                .withDefaultValue("0.0.0.0")
+                .build();
+
+        static final PropertyDescription<Integer> PORT = PropertyDescriptions
+                .integerProperty("port")
+                .withDefaultValue(6307)
+                .withValidator(Validators.portValidator())
+                .build();
+    }
+
     private final Undertow undertow;
 
     public HttpServer(Properties properties, AuthManager authManager, ReadStreamHandler readStreamHandler) {
-        String host = properties.getProperty("host", "0.0.0.0");
-        int port = PropertiesExtractor.get(properties, "port", 6307);
+        final String host = Props.HOST.extract(properties);
+        final int port = Props.PORT.extract(properties);
 
         HttpHandler handler = Handlers.routing()
                 .get("/ping", exchange -> {
