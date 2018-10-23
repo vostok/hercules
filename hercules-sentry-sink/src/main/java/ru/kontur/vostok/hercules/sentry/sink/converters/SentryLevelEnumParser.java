7e6ed7f62f883efcb8ebd2db102498ff03be42f1
Kirill Sulim <sulim@skbkontur.ru>
2018-10-23 16:25:32 +0500

    Use PropertyDescritption in hercules-sentry-sink

--- hercules-sentry-sink/src/main/java/ru/kontur/vostok/hercules/sentry/sink/converters/SentryLevelEnumParser.java
+++ hercules-sentry-sink/src/main/java/ru/kontur/vostok/hercules/sentry/sink/converters/SentryLevelEnumParser.java
@@ -2,6 +2,7 @@ package ru.kontur.vostok.hercules.sentry.sink.converters;
 
 import io.sentry.event.Event;
 import ru.kontur.vostok.hercules.util.enumeration.EnumUtil;
+import ru.kontur.vostok.hercules.util.functional.Result;
 
 import java.util.Optional;
 
@@ -15,6 +16,12 @@ public final class SentryLevelEnumParser {
     public static Optional<Event.Level> parse(String value) {
         value = prepareLevel(value);
         return EnumUtil.parseOptional(Event.Level.class, value);
+    }
+
+    public static Result<Event.Level, String> parseAsResult(String value) {
+        return parse(value)
+                .map(Result::<Event.Level, String>ok)
+                .orElseGet(() -> Result.error(String.format("Cannot parse '%s' as event level", value)));
     }
 
     /*
