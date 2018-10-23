b3fc9db67a86a6893ddf335d39c24de19cf6f37a
Kirill Sulim <sulim@skbkontur.ru>
2018-10-23 14:31:46 +0500

    Use PropertyDescription in hercules-gate-client

--- hercules-util/src/main/java/ru/kontur/vostok/hercules/util/properties/PropertyDescriptions.java
+++ hercules-util/src/main/java/ru/kontur/vostok/hercules/util/properties/PropertyDescriptions.java
@@ -2,6 +2,8 @@ package ru.kontur.vostok.hercules.util.properties;
 
 import ru.kontur.vostok.hercules.util.parsing.Parsers;
 
+import java.util.List;
+
 /**
  * PropertyDescriptions
  *
@@ -27,5 +29,17 @@ public final class PropertyDescriptions {
 
     public static <T> PropertyDescriptionBuilder<T> propertyOfType(Class<T> clazz, String name) {
         return PropertyDescriptionBuilder.start(name, clazz, null);
+    }
+
+    public static PropertyDescriptionBuilder<List<String>> listOfStringsProperty(String name) {
+        // Trick to work-around type erasure
+        // https://stackoverflow.com/a/30754982/2535153
+        @SuppressWarnings("unchecked")
+        Class<List<String>> clazz = (Class<List<String>>)(Class<?>)List.class;
+        return PropertyDescriptionBuilder.start(name, clazz, Parsers.parseList(Parsers::parseString));
+    }
+
+    public static PropertyDescriptionBuilder<String[]> arrayOfStringsProperty(String name) {
+        return PropertyDescriptionBuilder.start(name, String[].class, Parsers.parseArray(String.class, Parsers::parseString));
     }
 }
