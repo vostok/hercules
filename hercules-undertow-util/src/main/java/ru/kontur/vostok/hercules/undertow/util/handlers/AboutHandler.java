583e40676157682b80ee778ee2ff6e0d2a66a365
Kirill Sulim <sulim@skbkontur.ru>
2018-10-29 16:47:52 +0500

    Add maven git plugin

--- hercules-undertow-util/src/main/java/ru/kontur/vostok/hercules/undertow/util/handlers/AboutHandler.java
+++ hercules-undertow-util/src/main/java/ru/kontur/vostok/hercules/undertow/util/handlers/AboutHandler.java
@@ -3,11 +3,18 @@ package ru.kontur.vostok.hercules.undertow.util.handlers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.kontur.vostok.hercules.util.application.ApplicationContext;
 import ru.kontur.vostok.hercules.util.application.ApplicationContextHolder;
+import ru.kontur.vostok.hercules.util.properties.PropertyDescription;
+import ru.kontur.vostok.hercules.util.properties.PropertyDescriptions;
 import ru.kontur.vostok.hercules.util.throwable.ThrowableUtil;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 /**
  * AboutHandler
@@ -16,15 +23,43 @@ import java.nio.charset.StandardCharsets;
  */
 public class AboutHandler implements HttpHandler {
 
+    private static class Props {
+        static final PropertyDescription<String> COMMIT_HASH = PropertyDescriptions
+                .stringProperty("git.commit.id")
+                .withDefaultValue("unknown")
+                .build();
+
+        static final PropertyDescription<String> VERSION = PropertyDescriptions
+                .stringProperty("git.build.version")
+                .withDefaultValue("unknown")
+                .build();
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AboutHandler.class);
+
+    private static final String GIT_PROPERTIES = "git.properties";
+
     private final String aboutJsonString;
 
     public AboutHandler() {
         ApplicationContext applicationContext = ApplicationContextHolder.get();
 
+
+        Properties properties = new Properties();
+        try {
+            InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(GIT_PROPERTIES);
+            properties.load(resourceAsStream);
+        }
+        catch (IOException e) {
+            LOGGER.warn("Cannot load '{}' file", GIT_PROPERTIES, e);
+        }
+        final String commitHash = Props.COMMIT_HASH.extract(properties);
+        final String version = Props.VERSION.extract(properties);
+
         AboutInfo info = new AboutInfo(
                 applicationContext.getName(),
-                "",
-                "",
+                version,
+                commitHash,
                 applicationContext.getEnvironment(),
                 applicationContext.getInstanceId()
         );
