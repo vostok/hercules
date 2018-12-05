029ce93a47bb9ab0c90365f8f8b34b3aacc1dc5c
Gregory <kgn@skbkontur.ru>
2018-12-05 10:45:10 +0500

    Additional argument checks.

--- hercules-util/src/main/java/ru/kontur/vostok/hercules/util/Maps.java
+++ hercules-util/src/main/java/ru/kontur/vostok/hercules/util/Maps.java
@@ -15,10 +15,22 @@ public class Maps {
     public static final int HASH_MAP_MAXIMUM_CAPACITY = 1 << 30;
 
     public static int effectiveHashMapCapacity(int size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Illegal size: " + size);
+        }
         return effectiveHashMapCapacity(size, HASH_MAP_DEFAULT_LOAD_FACTOR);
     }
 
     public static int effectiveHashMapCapacity(int size, float loadFactor) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Illegal size: " + size);
+        }
+        if (size > HASH_MAP_MAXIMUM_CAPACITY) {
+            size = HASH_MAP_MAXIMUM_CAPACITY;
+        }
+        if (!(0 < loadFactor && loadFactor <= 1)) {
+            throw new IllegalArgumentException("Illegal load factor: " + loadFactor);
+        }
         float threshold = (size / loadFactor) + 1.0f;
         return (threshold < (float) HASH_MAP_MAXIMUM_CAPACITY)
                 ? (int) threshold
