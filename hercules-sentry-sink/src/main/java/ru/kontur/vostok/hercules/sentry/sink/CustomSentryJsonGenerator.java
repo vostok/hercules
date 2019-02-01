14f1e10dedc2057afef68a6a63b43a3c4cc7e0f9
Kirill Sulim <sulim@skbkontur.ru>
2019-02-01 16:35:23 +0500

    Add custom client realization to avoid stackTrace trimming

---
+++ hercules-sentry-sink/src/main/java/ru/kontur/vostok/hercules/sentry/sink/CustomSentryJsonGenerator.java
@@ -0,0 +1,20 @@
+package ru.kontur.vostok.hercules.sentry.sink;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import io.sentry.marshaller.json.SentryJsonGenerator;
+
+/**
+ * CustomSentryJsonGenerator
+ *
+ * @author Kirill Sulim
+ */
+public class CustomSentryJsonGenerator extends SentryJsonGenerator {
+
+    private static final int MAX_METAL_STRING_LENGTH = 200_000; // 200 for stack frame * 100 stack size * 10 exceptions
+
+    public CustomSentryJsonGenerator(JsonGenerator generator) {
+        super(generator);
+
+        super.setMaxLengthString(MAX_METAL_STRING_LENGTH);
+    }
+}
