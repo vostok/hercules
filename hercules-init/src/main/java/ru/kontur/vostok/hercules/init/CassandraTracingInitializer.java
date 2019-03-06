b49ef54cce8e918bd06bb973e918af15fa45380c
Gregory <kgn@skbkontur.ru>
2019-03-06 17:49:57 +0500

    Rename node property to nodes; Also, rename readTimeout to readTimeoutMs for CassandraConnector

--- hercules-init/src/main/java/ru/kontur/vostok/hercules/init/CassandraTracingInitializer.java
+++ hercules-init/src/main/java/ru/kontur/vostok/hercules/init/CassandraTracingInitializer.java
@@ -17,38 +17,7 @@ import java.util.concurrent.TimeUnit;
  * @author Gregory Koshelev
  */
 public class CassandraTracingInitializer {
-
-    private static class Props {
-        static final PropertyDescription<List<String>> NODES = PropertyDescriptions
-                .listOfStringsProperty("nodes")
-                .withDefaultValue(Collections.singletonList(CassandraDefaults.DEFAULT_CASSANDRA_ADDRESS))
-                .build();
-
-        static final PropertyDescription<Integer> PORT = PropertyDescriptions
-                .integerProperty("port")
-                .withDefaultValue(CassandraDefaults.DEFAULT_CASSANDRA_PORT)
-                .withValidator(Validators.portValidator())
-                .build();
-
-        static final PropertyDescription<String> KEYSPACE = PropertyDescriptions
-                .stringProperty("keyspace")
-                .withDefaultValue(CassandraDefaults.DEFAULT_KEYSPACE)
-                .build();
-
-        static final PropertyDescription<Short> REPLICATION_FACTOR = PropertyDescriptions
-                .shortProperty("replication.factor")
-                .withDefaultValue(CassandraDefaults.DEFAULT_REPLICATION_FACTOR)
-                .withValidator(Validators.greaterThan((short) 0))
-                .build();
-
-        static final PropertyDescription<Integer> TTL_SECONDS = PropertyDescriptions
-            .integerProperty("ttl.seconds")
-            .withDefaultValue((int) TimeUnit.DAYS.toSeconds(3))
-            .withValidator(Validators.greaterThan(0))
-            .build();
-    }
-
-    private final List<String> nodes;
+    private final String[] nodes;
     private final int port;
     private final String keyspace;
     private final short replicationFactor;
@@ -80,34 +49,64 @@ public class CassandraTracingInitializer {
             );
 
             session.execute(
-                "CREATE TABLE IF NOT EXISTS " + keyspace + ".tracing_spans (\n" +
-                    "        trace_id uuid,\n" +
-                    "        parent_span_id uuid,\n" +
-                    "        span_id uuid,\n" +
-                    "        payload blob,\n" +
-                    "        PRIMARY KEY (\n" +
-                    "            (trace_id),\n" +
-                    "            parent_span_id,\n" +
-                    "            span_id\n" +
-                    "        )\n" +
-                    "    )\n" +
-                    "    WITH\n" +
-                    "        CLUSTERING ORDER BY (parent_span_id ASC, span_id ASC)\n" +
-                    "        AND comment = 'Tracing span storage'\n" +
-                    "        AND default_time_to_live = " + ttl + "\n" +
-                    "        AND bloom_filter_fp_chance = 0.01\n" +
-                    "        AND compaction = {\n" +
-                    "            'class': 'org.apache.cassandra.db.compaction.TimeWindowCompactionStrategy',\n" +
-                    "            'compaction_window_unit': 'MINUTES',\n" +
-                    "            'compaction_window_size': '90'\n" +
-                    "        }\n" +
-                    "        AND compression = {\n" +
-                    "            'chunk_length_in_kb': '64',\n" +
-                    "            'class': 'org.apache.cassandra.io.compress.LZ4Compressor'\n" +
-                    "        }\n" +
-                    "        AND gc_grace_seconds = 10800\n" +
-                    "        AND read_repair_chance = 0.0;"
+                    "CREATE TABLE IF NOT EXISTS " + keyspace + ".tracing_spans (\n" +
+                            "        trace_id uuid,\n" +
+                            "        parent_span_id uuid,\n" +
+                            "        span_id uuid,\n" +
+                            "        payload blob,\n" +
+                            "        PRIMARY KEY (\n" +
+                            "            (trace_id),\n" +
+                            "            parent_span_id,\n" +
+                            "            span_id\n" +
+                            "        )\n" +
+                            "    )\n" +
+                            "    WITH\n" +
+                            "        CLUSTERING ORDER BY (parent_span_id ASC, span_id ASC)\n" +
+                            "        AND comment = 'Tracing span storage'\n" +
+                            "        AND default_time_to_live = " + ttl + "\n" +
+                            "        AND bloom_filter_fp_chance = 0.01\n" +
+                            "        AND compaction = {\n" +
+                            "            'class': 'org.apache.cassandra.db.compaction.TimeWindowCompactionStrategy',\n" +
+                            "            'compaction_window_unit': 'MINUTES',\n" +
+                            "            'compaction_window_size': '90'\n" +
+                            "        }\n" +
+                            "        AND compression = {\n" +
+                            "            'chunk_length_in_kb': '64',\n" +
+                            "            'class': 'org.apache.cassandra.io.compress.LZ4Compressor'\n" +
+                            "        }\n" +
+                            "        AND gc_grace_seconds = 10800\n" +
+                            "        AND read_repair_chance = 0.0;"
             );
         }
+    }
+
+    private static class Props {
+        static final PropertyDescription<String[]> NODES = PropertyDescriptions
+                .arrayOfStringsProperty("nodes")
+                .withDefaultValue(new String[]{CassandraDefaults.DEFAULT_CASSANDRA_ADDRESS})
+                .build();
+
+        static final PropertyDescription<Integer> PORT = PropertyDescriptions
+                .integerProperty("port")
+                .withDefaultValue(CassandraDefaults.DEFAULT_CASSANDRA_PORT)
+                .withValidator(Validators.portValidator())
+                .build();
+
+        static final PropertyDescription<String> KEYSPACE = PropertyDescriptions
+                .stringProperty("keyspace")
+                .withDefaultValue(CassandraDefaults.DEFAULT_KEYSPACE)
+                .build();
+
+        static final PropertyDescription<Short> REPLICATION_FACTOR = PropertyDescriptions
+                .shortProperty("replication.factor")
+                .withDefaultValue(CassandraDefaults.DEFAULT_REPLICATION_FACTOR)
+                .withValidator(Validators.greaterThan((short) 0))
+                .build();
+
+        static final PropertyDescription<Integer> TTL_SECONDS = PropertyDescriptions
+                .integerProperty("ttl.seconds")
+                .withDefaultValue((int) TimeUnit.DAYS.toSeconds(3))
+                .withValidator(Validators.greaterThan(0))
+                .build();
     }
 }
