be7e2e9cd740b30ce83cb8dfbe6bbe01e843ffd8
gnkoshelev <gnkoshelev@gmail.com>
2019-05-22 10:37:19 +0500

    Fix mime type value application/json

--- hercules-http/src/main/java/ru/kontur/vostok/hercules/http/MimeTypes.java
+++ hercules-http/src/main/java/ru/kontur/vostok/hercules/http/MimeTypes.java
@@ -6,7 +6,7 @@ package ru.kontur.vostok.hercules.http;
  * @author Gregory Koshelev
  */
 public class MimeTypes {
-    public static final String APPLICATION_JSON = "applicatiojn/json";
+    public static final String APPLICATION_JSON = "application/json";
     public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
     public static final String TEXT_PLAIN = "text/plain";
 
