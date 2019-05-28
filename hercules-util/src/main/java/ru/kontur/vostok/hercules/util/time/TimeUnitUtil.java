c1d6c1c4dbea91ec700bcacdd80802599ccfff0b
gnkoshelev <gnkoshelev@gmail.com>
2019-05-28 12:01:38 +0500

    Add previously stashed changes in TimeUnitUtil

--- hercules-util/src/main/java/ru/kontur/vostok/hercules/util/time/TimeUnitUtil.java
+++ hercules-util/src/main/java/ru/kontur/vostok/hercules/util/time/TimeUnitUtil.java
@@ -1,5 +1,7 @@
 package ru.kontur.vostok.hercules.util.time;
 
+import ru.kontur.vostok.hercules.util.Maps;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,16 +14,32 @@ import java.util.stream.Collectors;
  */
 public final class TimeUnitUtil {
     private static final Map<TimeUnit, String> unitToString;
+    private static final Map<TimeUnit, String> unitToBriefString;
 
     static {
         unitToString = Arrays.stream(TimeUnit.values()).
                 collect(Collectors.toMap(
                         Function.identity(),
                         unit -> unit.name().toLowerCase()));
+
+        Map<TimeUnit, String> map = new HashMap<>(Maps.effectiveHashMapCapacity(TimeUnit.values().length));
+        map.putAll(unitToString);
+        map.put(TimeUnit.NANOSECONDS, "ns");
+        map.put(TimeUnit.MICROSECONDS, "us");
+        map.put(TimeUnit.MILLISECONDS, "ms");
+        map.put(TimeUnit.SECONDS, "s");
+        map.put(TimeUnit.MINUTES, "min");
+        map.put(TimeUnit.HOURS, "hrs");
+        map.put(TimeUnit.DAYS, "days");
+        unitToBriefString = map;
     }
 
     public static String toString(TimeUnit unit) {
         return unitToString.get(unit);
+    }
+
+    public static String toBriefString(TimeUnit unit) {
+        return unitToBriefString.get(unit);
     }
 
     private TimeUnitUtil() {
