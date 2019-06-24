cdcac7f02bb251960236576fddac24f24371655b
demenev <demenev@skbkontur.ru>
2019-06-24 15:17:53 +0500

    optimize methods structure in SentrySyncProcessor

--- hercules-sentry-sink/src/main/java/ru/kontur/vostok/hercules/sentry/sink/SentrySyncProcessor.java
+++ hercules-sentry-sink/src/main/java/ru/kontur/vostok/hercules/sentry/sink/SentrySyncProcessor.java
@@ -45,18 +45,14 @@ public class SentrySyncProcessor {
     }
 
     /**
-     * Execute following operations: <p>
-     * - event filtering; <p>
-     * - getting Sentry client from the cache or creating Sentry client in Sentry and in the cache; <p>
-     * - converting Hercules event to Sentry event; <p>
-     * - sending event to Sentry. <p>
-     * The method handle different types of errors when executing this operations
+     * Process event
      *
      * @param event event
      * @return true if event is successfully processed
-     * or returns false if event is invalid or non retryable error occurred.
-     * @throws BackendServiceFailedException if retryable error occurred on every attempt of retry or
-     * other error occurred (which is not retryable or non retryable).
+     * or returns false if event is invalid or non retryable error occurred
+     * @throws BackendServiceFailedException if retryable error occurred on every attempt of retry,
+     * error has recommended waiting time
+     * or not described exception occurred
      */
     public boolean process(Event event) throws BackendServiceFailedException {
         final Optional<Level> level = ContainerUtil.extract(event.getPayload(), LogEventTags.LEVEL_TAG)
@@ -81,61 +77,114 @@ public class SentrySyncProcessor {
         Optional<String> sentryProjectName = ContainerUtil.extract(properties.get(), CommonTags.APPLICATION_TAG);
         String sentryProject = sentryProjectName.map(this::correctName).orElse(organization);
 
+        return tryToSend(event, organization, sentryProject);
+    }
+
+    /**
+     * Try to send the event to Sentry.
+     * The method executes retry if a retryable error has occurred
+     *
+     * @param event event to send
+     * @param organization Sentry organization where to send event
+     * @param sentryProject Sentry project where to send event
+     * @return true if event is successfully sent
+     * or returns false if a non retryable error occurred
+     * @throws BackendServiceFailedException if retryable error occurred on every attempt of retry,
+     * error has recommended waiting time
+     * or not described exception occurred
+     */
+    private boolean tryToSend(Event event, String organization, String sentryProject)
+            throws BackendServiceFailedException {
+        ErrorInfo previousErrorInfo = null;
         int retryCount = retryLimit;
         do {
             ErrorInfo processErrorInfo;
-            Result<SentryClient, ErrorInfo> sentryClient =
-                    sentryClientHolder.getOrCreateClient(organization, sentryProject);
-            if (!sentryClient.isOk()) {
-                LOGGER.error(String.format("Cannot get client for Sentry organization/project '%s/%s'",
-                        organization, sentryProject));
-                processErrorInfo = sentryClient.getError();
-                processErrorInfo.setIsRetryableForApiClient();
-                if (processErrorInfo.needDropAfterRetry() && retryCount == 0) {
-                    return false;
-                }
-            } else {
-                try {
-                    io.sentry.event.Event sentryEvent = SentryEventConverter.convert(event);
-                    sentryClient.get().sendEvent(sentryEvent);
-                    return true;
-                } catch (InvalidDsnException e) {
-                    LOGGER.error("InvalidDsnException: " + e.getMessage());
-                    processErrorInfo = new ErrorInfo(false);
-                } catch (LockedDownException e) {
-                    LOGGER.error("LockedDownException: a temporary lockdown is switched on");
-                    processErrorInfo = new ErrorInfo(true, LockdownManager.DEFAULT_BASE_LOCKDOWN_TIME);
-                } catch (ConnectionException e) {
-                    Integer responseCode = e.getResponseCode();
-                    String message = e.getMessage();
-                    if (responseCode != null) {
-                        LOGGER.error(String.format("ConnectionException: %d %s", responseCode, message));
-                        if (e.getRecommendedLockdownTime() != null) {
-                            processErrorInfo = new ErrorInfo(responseCode, e.getRecommendedLockdownTime());
-                        } else {
-                            processErrorInfo = new ErrorInfo(responseCode);
-                        }
-                    } else {
-                        LOGGER.error(String.format("ConnectionException: %s", message));
-                        throw new BackendServiceFailedException(e);
-                    }
-                } catch (Exception e) {
-                    throw new BackendServiceFailedException(e);
-                }
-                processErrorInfo.setIsRetryableForSending();
+            Result<Void, ErrorInfo> result = sendToSentry(event, organization, sentryProject);
+            if (result.isOk()) {
+                return true;
             }
-            if (processErrorInfo.isRetryable()) {
-                if (processErrorInfo.needToRemoveClientFromCache()) {
-                    sentryClientHolder.removeClientFromCache(organization, sentryProject);
-                }
-                if (processErrorInfo.getWaitingTimeMs() > 0) {
-                    throw new BackendServiceFailedException();
-                }
-            } else {
+            processErrorInfo = result.getError();
+            if (!processErrorInfo.isRetryable()) {
                 return false;
             }
-         } while (0 < retryCount--);
+            if (processErrorInfo.needDropAfterRetry() && retryCount == 0) {
+                return false;
+            }
+            if (processErrorInfo.needToRemoveClientFromCache()) {
+                sentryClientHolder.removeClientFromCache(organization, sentryProject);
+            }
+            if (processErrorInfo.getWaitingTimeMs() > 0) {
+                throw new BackendServiceFailedException();
+            }
+
+            if (previousErrorInfo != null) {
+                if (!processErrorInfo.equals(previousErrorInfo)) {
+                    retryCount = retryLimit;
+                }
+            }
+            previousErrorInfo = processErrorInfo;
+        } while (0 < retryCount--);
         throw new BackendServiceFailedException();
+    }
+
+    /**
+     * Execute following operations: <p>
+     * - getting Sentry client from the cache or creating Sentry client in Sentry and in the cache; <p>
+     * - converting Hercules event to Sentry event; <p>
+     * - sending event to Sentry. <p>
+     * The method handle different types of errors when executing this operations
+     *
+     * @param event event to send
+     * @param organization Sentry organization where to send event
+     * @param sentryProject Sentry project where to send event
+     * @return the {@link Result} object with error information in case of error
+     * @throws BackendServiceFailedException if not described exception occurred
+     */
+    private Result<Void, ErrorInfo> sendToSentry(Event event, String organization, String sentryProject)
+            throws BackendServiceFailedException {
+
+        ErrorInfo processErrorInfo;
+
+        Result<SentryClient, ErrorInfo> sentryClientResult =
+                sentryClientHolder.getOrCreateClient(organization, sentryProject);
+        if (!sentryClientResult.isOk()) {
+            LOGGER.error(String.format("Cannot get client for Sentry organization/project '%s/%s'",
+                    organization, sentryProject));
+            processErrorInfo = sentryClientResult.getError();
+            processErrorInfo.setIsRetryableForApiClient();
+            return Result.error(processErrorInfo);
+        }
+
+        try {
+            io.sentry.event.Event sentryEvent = SentryEventConverter.convert(event);
+            sentryClientResult.get().sendEvent(sentryEvent);
+            return Result.ok();
+        } catch (InvalidDsnException e) {
+            LOGGER.error("InvalidDsnException: " + e.getMessage());
+            processErrorInfo = new ErrorInfo(false);
+        } catch (LockedDownException e) {
+            LOGGER.error("LockedDownException: a temporary lockdown is switched on");
+            processErrorInfo = new ErrorInfo(true, LockdownManager.DEFAULT_BASE_LOCKDOWN_TIME);
+        } catch (ConnectionException e) {
+            Integer responseCode = e.getResponseCode();
+            String message = e.getMessage();
+            if (responseCode != null) {
+                LOGGER.error(String.format("ConnectionException: %d %s", responseCode, message));
+                if (e.getRecommendedLockdownTime() != null) {
+                    processErrorInfo = new ErrorInfo(responseCode, e.getRecommendedLockdownTime());
+                } else {
+                    processErrorInfo = new ErrorInfo(responseCode);
+                }
+            } else {
+                LOGGER.error(String.format("ConnectionException: %s", message));
+                throw new BackendServiceFailedException(e);
+            }
+        } catch (Exception e) {
+            throw new BackendServiceFailedException(e);
+        }
+        processErrorInfo.setIsRetryableForSending();
+
+        return Result.error(processErrorInfo);
     }
 
     private String correctName(String name) {
