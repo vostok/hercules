03db9808d5779189e25479a953a3b1220fc98948
gnkoshelev <gnkoshelev@gmail.com>
2019-08-02 02:16:57 +0500

    Add explicit exception handler

---
+++ hercules-http/src/main/java/ru/kontur/vostok/hercules/http/handler/ExceptionHandler.java
@@ -0,0 +1,28 @@
+package ru.kontur.vostok.hercules.http.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.kontur.vostok.hercules.http.HttpServerRequest;
+import ru.kontur.vostok.hercules.http.HttpStatusCodes;
+
+/**
+ * @author Gregory Koshelev
+ */
+public class ExceptionHandler implements HttpHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandler.class);
+
+    private final HttpHandler handler;
+
+    public ExceptionHandler(HttpHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void handle(HttpServerRequest request) {
+        try {
+            handler.handle(request);
+        } catch (Throwable throwable) {
+            request.complete(HttpStatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
