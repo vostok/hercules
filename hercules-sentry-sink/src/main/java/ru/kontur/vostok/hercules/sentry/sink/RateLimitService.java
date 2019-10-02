4a2585af6862741df57a138606ddf5878ece780a
a.zhdanov <a.zhdanov@skbkontur.ru>
2019-10-02 16:47:52 +0500

    fix rate limit calculation, fix props

--- hercules-sentry-sink/src/main/java/ru/kontur/vostok/hercules/sentry/sink/RateLimitService.java
+++ hercules-sentry-sink/src/main/java/ru/kontur/vostok/hercules/sentry/sink/RateLimitService.java
@@ -1,92 +1,71 @@
 package ru.kontur.vostok.hercules.sentry.sink;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ru.kontur.vostok.hercules.util.parameter.Parameter;
-import ru.kontur.vostok.hercules.util.parameter.ParameterValue;
 import ru.kontur.vostok.hercules.util.properties.PropertiesUtil;
-import ru.kontur.vostok.hercules.util.validation.IntegerValidators;
+import ru.kontur.vostok.hercules.util.validation.LongValidators;
 
-import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Artem Zhdanov
  */
 class RateLimitService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RateLimitService.class);
-
-    private final long timeWindowMs;
-    private final int maxRate;
+    private final long timeWindow;
+    private final long limit;
 
     private final ConcurrentHashMap<String, Bucket> buckets;
 
     RateLimitService(Properties properties) {
         final TimeUnit timeUnit = PropertiesUtil.get(Props.TIME_UNIT, properties).get();
-        this.timeWindowMs = timeUnit.toMillis(PropertiesUtil.get(Props.TIME_WINDOW, properties).get());
+        this.timeWindow = timeUnit.toMillis(PropertiesUtil.get(Props.TIME_WINDOW, properties).get());
 
-        this.maxRate = PropertiesUtil.get(Props.LIMIT, properties).get();
+        this.limit = PropertiesUtil.get(Props.LIMIT, properties).get();
         this.buckets = new ConcurrentHashMap<>();
     }
 
     /**
-     * @param key - key of rate limit state
+     * @param key - key of rate limit state.
      * @return true - if rate limit is not exceeded, else false
      */
     boolean updateAndCheck(String key) {
         if (key == null) {
             return false;
         }
-        return buckets.computeIfAbsent(key, s -> new Bucket(maxRate, timeWindowMs)).update() > -1;
-    }
-
-    float getValue(String key) {
-        return Optional.ofNullable(buckets.get(key)).map(Bucket::getValue).orElse(0f);
+        return buckets.computeIfAbsent(key, s -> new Bucket(limit, timeWindow)).update() >= 0;
     }
 
     private static class Bucket {
 
-        private final float maxRate;
-        private final long timeDenominator;
+        private final long limit;
+        private final long timeWindow;
 
-        private AtomicReference<Float> value;
-        private AtomicLong lastUpdate;
+        private final AtomicLong value;
+        private final AtomicLong lastUpdate;
 
-
-        Bucket(int maxRate, long timeWindow) {
-            this.maxRate = Integer.valueOf(maxRate).floatValue();
-            this.value = new AtomicReference<>(this.maxRate);
-            this.timeDenominator = timeWindow;
-
+        Bucket(long limit, long timeWindow) {
+            this.limit = limit;
+            this.timeWindow = timeWindow;
+            this.value = new AtomicLong(this.limit * this.timeWindow);
             this.lastUpdate = new AtomicLong(System.currentTimeMillis());
         }
 
-        int update() {
+        long update() {
             long now = System.currentTimeMillis();
             long delta = now - lastUpdate.getAndUpdate(operand -> now);
-            float increase = delta * maxRate / timeDenominator;
-            float updatedValue = value.updateAndGet(val -> Math.max(-1, Math.min(maxRate, increase + val - 1)));
-
-            LOGGER.trace("Rate Limit Service value = {}, delta = {}, increase = {}", updatedValue, delta, increase);
-
-            return Math.round(updatedValue);
-        }
-
-        float getValue() {
-            return value.get();
+            long increase = delta * limit - timeWindow;
+            return value.updateAndGet(val -> Math.max(-1, Math.min(val + increase, limit * timeWindow)));
         }
     }
 
     private static class Props {
-        static final Parameter<Integer> LIMIT = Parameter
-                .integerParameter("limit")
-                .withDefault(1000)
-                .withValidator(IntegerValidators.positive())
+        static final Parameter<Long> LIMIT = Parameter
+                .longParameter("limit")
+                .withDefault(1000L)
+                .withValidator(LongValidators.positive())
                 .build();
 
         static final Parameter<Long> TIME_WINDOW = Parameter
@@ -95,9 +74,7 @@ class RateLimitService {
                 .build();
 
         static final Parameter<TimeUnit> TIME_UNIT = Parameter
-                .parameter("timeUnit", val -> Optional.ofNullable(val)
-                        .map(v -> ParameterValue.of(TimeUnit.valueOf(v)))
-                        .orElse(ParameterValue.empty()))
+                .enumParameter("timeUnit", TimeUnit.class)
                 .withDefault(TimeUnit.MINUTES)
                 .build();
 
--- hercules-sentry-sink/src/test/java/ru/kontur/vostok/hercules/sentry/sink/RateLimitServiceTest.java
+++ hercules-sentry-sink/src/test/java/ru/kontur/vostok/hercules/sentry/sink/RateLimitServiceTest.java
@@ -17,81 +17,53 @@ public class RateLimitServiceTest {
     private static final String KEY = "SOME_PROJECT_KEY";
 
     /**
-     * maxRate = 1 per minute
+     * limit = 1 per minute
      * 1th event -> true
      * 2th event -> false
      */
     @Test
-    public void shouldBeTrueMaxRate1() {
+    public void shouldBeTrueWithLimit1PerMin() {
         RateLimitService service = createLimiter(1);
         assertTrue(service.updateAndCheck(KEY));
         assertFalse(service.updateAndCheck(KEY));
     }
 
     /**
-     * maxRate = 1 per sec
+     * limit = 1000 per minute
+     * 1 - 1000th event -> true
+     * 1001th event -> false
+     */
+    @Test
+    public void shouldBeTrueWithLimit1000PerMin() {
+        RateLimitService service = createLimiter(1000);
+        for (int i = 0; i < 1000; i++) {
+            assertTrue(service.updateAndCheck(KEY));
+        }
+        assertFalse(service.updateAndCheck(KEY));
+    }
+
+    /**
+     * limit = 1 per sec
      * 1th event -> true
      * 2th event -> false
      */
     @Test
-    public void shouldBeTrueMaxRate1Sec() {
+    public void shouldBeTrueWithLimit1PerSec() {
         RateLimitService service = createLimiter(1, 1, TimeUnit.SECONDS);
         assertTrue(service.updateAndCheck(KEY));
         assertFalse(service.updateAndCheck(KEY));
     }
 
     /**
-     * maxRate = 3 per minute
-     * 1 - 3th per sec event -> true
-     * 4th event -> false
-     */
-    @Test
-    public void shouldBeTrueMaxRate3() throws InterruptedException {
-        RateLimitService service = createLimiter(3);
-        int n = 3;
-        while (n-- > 0) {
-            assertTrue(service.updateAndCheck(KEY));
-            sleep(900);
-        }
-        assertFalse(service.updateAndCheck(KEY));
-    }
-
-    /**
-     * maxRate = 1000 per minute, with some threads
-     */
-    @Test
-    public void shouldBeTrueWithSomeThreads() throws InterruptedException {
-        RateLimitService service = createLimiter(1000);
-        Runnable runnable = () -> {
-            for (int n = 0; n < 200; n++) {
-                service.updateAndCheck(KEY);
-            }
-        };
-        new Thread(runnable).start();
-        new Thread(runnable).start();
-        new Thread(runnable).start();
-        new Thread(runnable).start();
-        new Thread(runnable).start();
-        sleep(1000);
-        float val = service.getValue(KEY);
-        assertTrue(val > -1 && val < 10);
-        System.out.println("Value: " + service.getValue(KEY));
-    }
-
-    /**
-     * maxRate = 1 per sec
+     * limit = 1 per sec
      * 1 - 4th event -> true
      * 5th event -> false
-     * increase should be = 1
-     * with some threads
      */
     @Test
-    public void shouldBeTrueMaxRate1PerSecWithIncrease1() throws InterruptedException {
-        RateLimitService service = createLimiter(1, 1L, TimeUnit.SECONDS);
-        Runnable runnable = () -> assertTrue(service.updateAndCheck(KEY));
-        int n = 3;
-        while (n-- > 0) {
-            new Thread(runnable).start();
+    public void shouldBeTrueWithLimit1PerSecAndCorrectIncrease() throws InterruptedException {
+        RateLimitService service = createLimiter(1, 1, TimeUnit.SECONDS);
+        for (int i = 0; i < 3; i++) {
+            assertTrue(service.updateAndCheck(KEY));
             sleep(1000);
         }
         assertTrue(service.updateAndCheck(KEY));
@@ -99,22 +71,22 @@ public class RateLimitServiceTest {
     }
 
     @Test(expected = IllegalStateException.class)
-    public void shouldBeExceptionWithIncorrectProperties() {
+    public void shouldBeExceptionWithNegativeLimit() {
         createLimiter(-1);
     }
 
     @Test(expected = IllegalStateException.class)
-    public void shouldBeExceptionWithIncorrectProperties2() {
+    public void shouldBeExceptionWithZeroLimit() {
         createLimiter(0, 0, TimeUnit.SECONDS);
     }
 
-    private RateLimitService createLimiter(int maxRate) {
-        return createLimiter(maxRate, 0, null);
+    private RateLimitService createLimiter(long limit) {
+        return createLimiter(limit, 0, null);
     }
 
-    private RateLimitService createLimiter(int maxRate, long timeWindow, TimeUnit timeUnit) {
+    private RateLimitService createLimiter(long limit, long timeWindow, TimeUnit timeUnit) {
         Properties properties = new Properties();
-        properties.setProperty("limit", String.valueOf(maxRate));
+        properties.setProperty("limit", String.valueOf(limit));
         if (timeWindow != 0) {
             properties.setProperty("timeWindow", String.valueOf(timeWindow));
         }
