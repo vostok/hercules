a175666497d2bd650b75c2c8b3a013cc2f645d51
Цыпаев Владимир Николаевич <tsypaev@skbkontur.ru>
2020-01-13 15:38:12 +0500

    Resolve "Enrich logs in Gate"

--- hercules-gate/src/main/java/ru/kontur/vostok/hercules/gate/validation/EventValidator.java
+++ hercules-gate/src/main/java/ru/kontur/vostok/hercules/gate/validation/EventValidator.java
@@ -1,17 +1,23 @@
 package ru.kontur.vostok.hercules.gate.validation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.kontur.vostok.hercules.protocol.Event;
 
 /**
  * @author Gregory Koshelev
  */
 public class EventValidator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventValidator.class);
+
     public boolean validate(Event event) {
         if (event.getVersion() != 1) { // Gate supports Event of version 1 only
+            LOGGER.warn("Event version != 1");
             return false;
         }
 
         if (event.getTimestamp() < 0) { // Event timestamp should be non-negative
+            LOGGER.warn("Event timestamp < 0");
             return false;
         }
 
